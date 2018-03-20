df039963b7389e1b4414e54a20608ec2c4c57a04
Gutemberg Felipe de Oliveira <gutemberg.oliveira@gzvr.com.br>
2018-03-20 19:28:56 -0300

    funcao alterar

--- aula6_python2.py
+++ aula6_python2.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 def cadastrar(nomes):
     print 'Digite: o nome:'
     nome = raw_input()
@@ -7,7 +9,7 @@ def menu():
     nomes = []
     escolha = ''
     while(escolha != '0'):
-        print 'Digite: 1 para cadastrar, 2 para listar, 0 para terminar'
+        print 'Digite: 1 para cadastrar, 2 para listar, 3 para remover, 4 alterar,  0 para terminar'
         escolha = raw_input()
 
         if(escolha == '1'):
@@ -16,10 +18,31 @@ def menu():
         if(escolha == '2'):
             listar(nomes)
 
+        if escolha == '3':
+            remover(nomes)
+
+        if escolha == '4':
+            alterar(nomes)
+
+
 def listar(nomes):
     print 'Listando nomes:'
     for nome in nomes:
         print nome
+
+def remover(nomes):
+    print 'qual nome você deseja remover'
+    nome = raw_input()
+    nomes.remove(nome)
+
+def alterar(nomes):
+    print 'Qual nome vc gostaria de alterar?'
+    nome = raw_input()
+
+    if(nome in nomes):
+        indice = nomes.index(nome)
+        alterado = raw_input()
+        nomes[indice] = alterado
 
 
 menu()
